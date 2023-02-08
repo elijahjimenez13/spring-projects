4fec6cfb0a90777ec9d4a4d374314559a5bcdb9f
MasterEezzy <iamelijahjimenez@gmail.com>
2023-02-07 19:45:26 -0800

    fixed hangman error

--- Python/Hangman/hang.py
+++ Python/Hangman/hang.py
@@ -2,51 +2,41 @@ import random
 from words import words
 import string
 
-
 def get_valid_word(words):
     word = random.choice(words) #randomly chooses something from the list
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
     return word
-
-
 def hangman():
+    #user_letter - is user input letter
+    #alphabet - is the entire Alphabet 
+    #used_letters - Letters we have already used
+    #word_letters - letters for word
+    #word_list - Dashes
+
     word = get_valid_word(words)
     print(word)
-    word_letters = set(word) # letters in the word
-    alphabet = set(string.ascii_uppercase)
+    word_letters = set(word.lower()) # letters in the word
+    alphabet = set(string.ascii_lowercase)
     used_letters = set() # what the user has guess
-    print(word_letters)
-
+   
     # getting user input
     while len(word_letters) > 0:
         # letters used
         # ' ' .join['a', 'b', 'cd'] ---> 'a b cd"
         print('You have used these letters: ', ' '.join(used_letters))
-
         # what current word is (ie W - R D)
-       
-        word_list = ["a"  if 'e' in used_letters  else '-' for letter in word]
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        
         print('Current word: ', ' '.join(word_list))
 
-        user_letter = input('Guess a letter: ').upper()
-        #user_letter - is user input letter
-        #alphabet - is the entire Alphabet 
-        #used_letters - Letters we have already used
-        #word_letters - letters for word
-        #word_list - Dashes
+        user_letter = input('Guess a letter: ').lower()
+        
         if user_letter in alphabet - used_letters:
-          
             used_letters.add(user_letter)
-
-
-           
-
-            if user_letter.lower() in word_letters:
-                print('testing debug')
-                word_letters.remove(user_letter.lower())
-                print(word_letters)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
         elif user_letter in used_letters:
             print('You already guessed this letter. Please try again.')
