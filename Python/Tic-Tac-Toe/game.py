a54e3165da4ed4632500310c9255866268effb74
MasterEezzy <iamelijahjimenez@gmail.com>
2023-03-08 20:31:41 -0800

    finished first tic-tac-toe

--- Python/Tic-Tac-Toe/game.py
+++ Python/Tic-Tac-Toe/game.py
@@ -1,3 +1,4 @@
+import time
 from player import HumanPlayer, RandomComputerPlayer
 
 class TicTacToe:
@@ -100,6 +101,9 @@ def play(game, x_player, o_player, print_game = True):
                 # letter = 'O'
             # else:
                 # letter = 'X'
+
+        # tiny break to make things easier to read
+        time.sleep(0.8)
 
         if print_game:
             print('It\'s a tie!')
